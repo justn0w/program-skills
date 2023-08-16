ad0daf33f5766c1c971905d239b14706b3514963
justn0w <ggjustnow@163.com>
2023-08-17 01:40:53 +0800

    Consumer代码式例

--- program-skills-jdk8new/src/main/java/com/justnow/skills/learnFunctionalInterface/FunctionInterfacePredicate.java
+++ program-skills-jdk8new/src/main/java/com/justnow/skills/learnFunctionalInterface/FunctionInterfacePredicate.java
@@ -3,6 +3,7 @@ package com.justnow.skills.learnFunctionalInterface;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 /**
@@ -13,7 +14,9 @@ import java.util.function.Predicate;
 public class FunctionInterfacePredicate {
 
     public static void main(String[] args) {
-        predicateDemo01();
+        //predicateDemo01();
+
+        consumerDemo01();
     }
 
     public static <T> List<T> filter(List<T> list, Predicate<T> p) {
@@ -31,12 +34,35 @@ public class FunctionInterfacePredicate {
         List<String> list = Arrays.asList("1", "2", "3", "", "4");
         System.out.println("过滤前：" + list.toString());
         List<String> filter = filter(list, nonEmptyFilter);
-
         //简洁模式
         List<String> filter1 = filter(list, s -> !s.isEmpty());
+
         System.out.println("过滤后：" + filter.toString());
         System.out.println("过滤后：" + filter1.toString());
 
+    }
+
+    public static void consumerDemo01() {
+        List<String> lists = Arrays.asList("1", "2", "3", "4", "5");
+
+        //1 函数式接口的实例
+        Consumer<String> consumerPrint =  (String s) -> System.out.println(s);
+
+        //2 使用函数式接口做为入参
+        forEach(lists, consumerPrint);
+
+        //3 简洁模式
+        forEach(lists, (String s) -> System.out.println(s));
+
+        //4 使用方法引用优化lambda表达式
+        forEach(lists, System.out::println);
+
+    }
+
+    public static <T> void forEach(List<T> list, Consumer<T> c) {
+        for (T t : list) {
+            c.accept(t);
+        }
     }
 
 }
